54b7415e365f46fcf4c2ee10698eea887e13c5e1
kaushikme1 <71588177+kaushikme1@users.noreply.github.com>
2021-06-04 20:41:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react-native';
+import ArticleScreen from './screens/Article';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <ArticleScreen/>
+    )
+  }
+}
